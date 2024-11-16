20de07bd43c0e595b0327dca9840ad0372766ac3
Tenketem Zeradawit <titihadas123@gmail.com>
2024-11-16 11:05:53 -0800

    first commit

---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./card.css"
+
+function Card(props) {
+  return (
+    
+    <div className="card">
+      <img src={props.image} alt="Card image" />
+      <h2>{props.title}</h2>
+    <hr />
+      <p> Description:{props.description}</p>
+    </div>
+  );
+}
+    
+export default Card
